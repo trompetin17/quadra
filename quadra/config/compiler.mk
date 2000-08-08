e01ae0268c9b5d38da72e4f42bfa3986057ede80
pphaneuf <pphaneuf@8926ee93-8b47-0410-9975-e57e0fb0bd0c>
2000-08-08 04:16:46 +0000

    Initial commit.

--- quadra/config/compiler.mk
+++ quadra/config/compiler.mk
@@ -3,8 +3,10 @@
 
 include config/target.mk
 
-ARFLAGS:=rcs
-CXXFLAGS+=-pipe -pedantic -Wall -Iinclude -I../images
+SKELTON:=skelton
+
+CXXFLAGS+=-pipe -pedantic -Wall -Iinclude -I$(SKELTON)/include
+LDFLAGS+=-L$(SKELTON)/lib
 
 OPTFLAGS+=-m486 -O6
 DEBUGFLAGS=-ggdb
@@ -23,13 +25,12 @@ endif
 
 ifeq "$(TARGET)" "linux"
 CXXFLAGS+=-DUGS_LINUX -DUGS_LINUX_SVGA -DUGS_LINUX_X11
-LDLIBS+=-L/usr/X11R6/lib -lX11 -lXext -lXpm -lvga -lvgagl -lz
-ifdef RELEASE
-LDLIBS+=-ldb
-else
-LDLIBS+=-ldb1
-endif
+#LDLIBS+=-L/usr/X11R6/lib -lX11 -lXext -lXpm -lvga -lvgagl -lz
+#ifdef RELEASE
+#LDLIBS+=-ldb
+#else
+#LDLIBS+=-ldb1
+#endif
 else
 # hmm, nothing else than linux is supported by this Makefile!
 endif
-
