9b429e7797611a45df7c2c36911ad29037740573
pphaneuf <pphaneuf@8926ee93-8b47-0410-9975-e57e0fb0bd0c>
2001-03-12 19:01:10 +0000

    Added a temporary PHP Quadra master server.

---
+++ trunk/quadra-www/htdocs/qserv/index.php
@@ -0,0 +1,186 @@
+<? header("Content-Type: text/plain");
+print "Current games\n";
+
+print "127.0.0.1/name !!! IMPORTANT !!!
+127.0.0.1/rules/survivor 0
+127.0.0.1/rules/hot_potato 0
+127.0.0.1/rules/type 0
+127.0.0.1/rules/mincombo 15
+127.0.0.1/rules/levelstart 99
+127.0.0.1/rules/allowhandicap 1
+127.0.0.1/rules/attacks/normal 0 0
+127.0.0.1/rules/attacks/clean 0 0
+127.0.0.1/rules/levelup 0
+127.0.0.1/status/terminated 0
+127.0.0.1/status/autorestart 0
+127.0.0.1/status/started 0
+127.0.0.1/status/running_time 175031
+127.0.0.1/info/quadra_version 1.1.5
+127.0.0.1/info/language 0
+127.0.0.1/info/platform/os Linux i386
+127.0.0.1/info/platform/display Xlib
+127.0.0.1/info/registered 1
+127.0.0.1/info/remoteaddr 127.0.0.1
+127.0.0.1/port 3456
+127.0.0.1/version 23
+127.0.0.1/stats/round_number 0
+127.0.0.1/stats/playing_time 0
+127.0.0.1/end/type 0
+127.0.0.1/end/value 1
+127.0.0.2/name  
+127.0.0.2/rules/survivor 0
+127.0.0.2/rules/hot_potato 0
+127.0.0.2/rules/type 0
+127.0.0.2/rules/mincombo 15
+127.0.0.2/rules/levelstart 99
+127.0.0.2/rules/allowhandicap 1
+127.0.0.2/rules/attacks/normal 0 0
+127.0.0.2/rules/attacks/clean 0 0
+127.0.0.2/rules/levelup 0
+127.0.0.2/status/terminated 0
+127.0.0.2/status/autorestart 0
+127.0.0.2/status/started 0
+127.0.0.2/status/running_time 175031
+127.0.0.2/info/quadra_version 1.1.5
+127.0.0.2/info/language 0
+127.0.0.2/info/platform/os Linux i386
+127.0.0.2/info/platform/display Xlib
+127.0.0.2/info/registered 1
+127.0.0.2/info/remoteaddr 127.0.0.2
+127.0.0.2/port 3456
+127.0.0.2/version 23
+127.0.0.2/stats/round_number 0
+127.0.0.2/stats/playing_time 0
+127.0.0.2/end/type 0
+127.0.0.2/end/value 1
+127.0.0.3/name Quadra master server
+127.0.0.3/rules/survivor 0
+127.0.0.3/rules/hot_potato 0
+127.0.0.3/rules/type 0
+127.0.0.3/rules/mincombo 15
+127.0.0.3/rules/levelstart 99
+127.0.0.3/rules/allowhandicap 1
+127.0.0.3/rules/attacks/normal 0 0
+127.0.0.3/rules/attacks/clean 0 0
+127.0.0.3/rules/levelup 0
+127.0.0.3/status/terminated 0
+127.0.0.3/status/autorestart 0
+127.0.0.3/status/started 0
+127.0.0.3/status/running_time 175031
+127.0.0.3/info/quadra_version 1.1.5
+127.0.0.3/info/language 0
+127.0.0.3/info/platform/os Linux i386
+127.0.0.3/info/platform/display Xlib
+127.0.0.3/info/registered 1
+127.0.0.3/info/remoteaddr 127.0.0.3
+127.0.0.3/port 3456
+127.0.0.3/version 23
+127.0.0.3/stats/round_number 0
+127.0.0.3/stats/playing_time 0
+127.0.0.3/end/type 0
+127.0.0.3/end/value 1
+127.0.0.4/name is down. Get all the
+127.0.0.4/rules/survivor 0
+127.0.0.4/rules/hot_potato 0
+127.0.0.4/rules/type 0
+127.0.0.4/rules/mincombo 15
+127.0.0.4/rules/levelstart 99
+127.0.0.4/rules/allowhandicap 1
+127.0.0.4/rules/attacks/normal 0 0
+127.0.0.4/rules/attacks/clean 0 0
+127.0.0.4/rules/levelup 0
+127.0.0.4/status/terminated 0
+127.0.0.4/status/autorestart 0
+127.0.0.4/status/started 0
+127.0.0.4/status/running_time 175031
+127.0.0.4/info/quadra_version 1.1.5
+127.0.0.4/info/language 0
+127.0.0.4/info/platform/os Linux i386
+127.0.0.4/info/platform/display Xlib
+127.0.0.4/info/registered 1
+127.0.0.4/info/remoteaddr 127.0.0.4
+127.0.0.4/port 3456
+127.0.0.4/version 23
+127.0.0.4/stats/round_number 0
+127.0.0.4/stats/playing_time 0
+127.0.0.4/end/type 0
+127.0.0.4/end/value 1
+127.0.0.5/name information on our
+127.0.0.5/rules/survivor 0
+127.0.0.5/rules/hot_potato 0
+127.0.0.5/rules/type 0
+127.0.0.5/rules/mincombo 15
+127.0.0.5/rules/levelstart 99
+127.0.0.5/rules/allowhandicap 1
+127.0.0.5/rules/attacks/normal 0 0
+127.0.0.5/rules/attacks/clean 0 0
+127.0.0.5/rules/levelup 0
+127.0.0.5/status/terminated 0
+127.0.0.5/status/autorestart 0
+127.0.0.5/status/started 0
+127.0.0.5/status/running_time 175031
+127.0.0.5/info/quadra_version 1.1.5
+127.0.0.5/info/language 0
+127.0.0.5/info/platform/os Linux i386
+127.0.0.5/info/platform/display Xlib
+127.0.0.5/info/registered 1
+127.0.0.5/info/remoteaddr 127.0.0.5
+127.0.0.5/port 3456
+127.0.0.5/version 23
+127.0.0.5/stats/round_number 0
+127.0.0.5/stats/playing_time 0
+127.0.0.5/end/type 0
+127.0.0.5/end/value 1
+127.0.0.6/name web site at
+127.0.0.6/rules/survivor 0
+127.0.0.6/rules/hot_potato 0
+127.0.0.6/rules/type 0
+127.0.0.6/rules/mincombo 15
+127.0.0.6/rules/levelstart 99
+127.0.0.6/rules/allowhandicap 1
+127.0.0.6/rules/attacks/normal 0 0
+127.0.0.6/rules/attacks/clean 0 0
+127.0.0.6/rules/levelup 0
+127.0.0.6/status/terminated 0
+127.0.0.6/status/autorestart 0
+127.0.0.6/status/started 0
+127.0.0.6/status/running_time 175031
+127.0.0.6/info/quadra_version 1.1.5
+127.0.0.6/info/language 0
+127.0.0.6/info/platform/os Linux i386
+127.0.0.6/info/platform/display Xlib
+127.0.0.6/info/registered 1
+127.0.0.6/info/remoteaddr 127.0.0.6
+127.0.0.6/port 3456
+127.0.0.6/version 23
+127.0.0.6/stats/round_number 0
+127.0.0.6/stats/playing_time 0
+127.0.0.6/end/type 0
+127.0.0.6/end/value 1
+127.0.0.7/name quadra.sourceforge.net!
+127.0.0.7/rules/survivor 0
+127.0.0.7/rules/hot_potato 0
+127.0.0.7/rules/type 0
+127.0.0.7/rules/mincombo 15
+127.0.0.7/rules/levelstart 99
+127.0.0.7/rules/allowhandicap 1
+127.0.0.7/rules/attacks/normal 0 0
+127.0.0.7/rules/attacks/clean 0 0
+127.0.0.7/rules/levelup 0
+127.0.0.7/status/terminated 0
+127.0.0.7/status/autorestart 0
+127.0.0.7/status/started 0
+127.0.0.7/status/running_time 175031
+127.0.0.7/info/quadra_version 1.1.5
+127.0.0.7/info/language 0
+127.0.0.7/info/platform/os Linux i386
+127.0.0.7/info/platform/display Xlib
+127.0.0.7/info/registered 1
+127.0.0.7/info/remoteaddr 127.0.0.7
+127.0.0.7/port 3456
+127.0.0.7/version 23
+127.0.0.7/stats/round_number 0
+127.0.0.7/stats/playing_time 0
+127.0.0.7/end/type 0
+127.0.0.7/end/value 1";
+?>
